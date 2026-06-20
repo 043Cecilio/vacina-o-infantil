40099d62cc4e83e9b6643054c2ef8864dc2fe7df
043Cecilio <gabrielcecilio@alunos.utfpr.edu.br>
2026-06-20 03:01:50 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vacina-infantil',
+  webDir: 'www'
+};
+
+export default config;
